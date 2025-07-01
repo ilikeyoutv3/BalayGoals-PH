731889982d9227a9570227bfb70bd4b5a8852402
ilikeyoutv3 <ilikeyoutv3@gmail.com>
2025-07-01 12:13:29 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        realestate: {
+          light: '#f5f9ff',
+          DEFAULT: '#c7d2fe',
+          dark: '#4f46e5',
+        },
+      },
+    },
   },
   plugins: [],
 }
